c136d7c5796ed23b92fc937ab5775904a56d01d6
Kirill F <kirill.olegovich31@gmail.com>
2023-03-12 22:00:55 +0300

    'Integrating sqlite' section 5 done

--- code/item.py
+++ code/item.py
@@ -1,22 +1,27 @@
 import sqlite3
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, request
 from flask_jwt import jwt_required
 
 
-
 class Item(Resource):
-
-
+    parser = reqparse.RequestParser()
+    parser.add_argument("price",
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank")
 
     @jwt_required() # this is a speciall callable decorator that returns a decorator when called (e.g. some_method = some_decorator()(some_method))
     def get(self, name):
-        # for item in items:
-        #     if item['name'] == name:
-        #        return item
+        item = Item.findByName(name)
+        if item:
+            return item
+        
+        return {
+            'message': 'Item not found'
+        }, 404
 
-        # item = next(filter(lambda x: x["name"] == name, items), None) # when lambda function returns True the item is from items is added to the filter object, and next get the first and only object
-        # return {'item': item}, 200 if item else 404
-
+    @classmethod
+    def findByName(cls, name):
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -32,49 +37,85 @@ class Item(Resource):
                 'price': row[1]
                 }
             }
+    
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item["name"], item["price"]))
+
+        connection.commit()
+        connection.close()
+    
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        cursor.execute("UPDATE items SET price=? WHERE name=?", (item["price"], item["name"]))
         
-        return {
-            'message': 'Item not found'
-        }, 404
-        
-
-
-
+        connection.commit()
+        connection.close()
 
     def post(self, name):
-        if next(filter(lambda x: x["name"] == name, items), None) is not None:
-            return {"message": "an item with name {} already exists".format(items["name"])}, 400
+        if Item.findByName(name):
+            return {"message": "an item with name {} already exists".format(name)}, 400
+
         data = request.get_json() # this allows for your api to access the request's body payload in json
         price = data["price"]
         item = {'name': name, 'price': price}
-        items.append(item)
+        try:
+            Item.insert(item)
+        except:
+            return {"message": "Something went wrong trying to insert the item into DB"}, 500
+
         return item, 201
 
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x["name"] != name, items))
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        
+        query = "DELETE FROM items WHERE name=?"
+
+        cursor.execute(query, (name,))
+        connection.commit()
+        connection.close()
+
         return {"message": "item deleted"}
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-                            type=float,
-                            required=True,
-                            help="This field cannot be left blank!")
+        data = Item.parser.parse_args() #defined at the beginning of the class
 
-        # data = request.get_json() # once you add reqparse.RequestParser() this line should be modified because you will get the payload from the parser now
-        data = parser.parse_args()
-        item = next(filter(lambda x: x["name"] == name, items), None)
+        item = Item.findByName(name)
+        updated_item = {"name": name, "price": data["price"]}
         if item is None:
-            item = {"name": name, "price": data["price"]}
-            items.append(item)
+            try:
+                Item.insert(updated_item)
+            except:
+                return {"message": "An error occured inserting an item"}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                Item.update(updated_item)
+            except:
+                return {"message": "An error occured updating an item"}, 500
+
+        return updated_item
 
 
 
 
-class Items(Resource):
+class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        rows = cursor.execute("SELECT * FROM items").fetchall()
+        items = []
+
+        for row in rows:
+            items.append({"name": row[0], "price": row[1]})
+        
+        connection.close()
+
         return {"items": items}, 200
